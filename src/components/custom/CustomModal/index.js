95797c00e79a33995cb24b8eb485d5b9b8274e5c
GustavoGSantos <gustavogimenezs@icloud.com>
2019-03-06 22:26:44 -0300

    Correção de identação

--- src/components/custom/CustomModal/index.js
+++ src/components/custom/CustomModal/index.js
@@ -4,22 +4,22 @@ import Button from 'react-bootstrap/Button';
 import ReactDOM from 'react-dom';
 
 const CustomModal = () => (
-          <div>          
-        <Modal id="custom-modal" isOpen={this.state.open} onRequestClose={this.closeModal}> {/*show={this.state.show} onHide={this.handleClose}>*/}
-            <Modal.Header closeButton>
-              <Modal.Title>Modal heading</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-            <Modal.Footer>
-              <Button variant="secondary" onClick={this.handleClose}>
-                Close
+  <div>
+    <Modal id="custom-modal" isOpen={this.state.open} onRequestClose={this.closeModal}> {/*show={this.state.show} onHide={this.handleClose}>*/}
+      <Modal.Header closeButton>
+        <Modal.Title>Modal heading</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={this.handleClose}>
+          Close
               </Button>
-              <Button variant="primary" onClick={this.handleClose}>
-                Save Changes
+        <Button variant="primary" onClick={this.handleClose}>
+          Save Changes
               </Button>
-            </Modal.Footer>
-          </Modal>
-          </div>
+      </Modal.Footer>
+    </Modal>
+  </div>
 );
 
-  export default CustomModal;
+export default CustomModal;
