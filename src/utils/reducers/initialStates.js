4aafe82865d90404130967d441a1342e8033c6e0
Henrique Guerra <hfranca@outlook.com>
2019-03-24 00:26:47 -0300

    fix access

--- src/utils/reducers/initialStates.js
+++ src/utils/reducers/initialStates.js
@@ -12,7 +12,7 @@ export const accessState = {
 	accessHasSucceded: false,
 	accessRequestHasFailed: false,
 	accessRequestIsPending: false,
-	userId: "4"
+	userId: ""
 };
 
 export const bookState = {
