159a4b4b2f0efc6c1cd36307a75e543b349177da
GustavoGSantos <gustavogimenezs@icloud.com>
2019-03-26 00:12:38 -0300

    Textos das páginas e alinhamento do mesmo

--- src/pages/How/index.js
+++ src/pages/How/index.js
@@ -3,17 +3,33 @@ import { connect } from 'react-redux';
 
 import App from '../../components/custom/App';
 
-class Us extends React.Component {
+class How extends React.Component {
     render() {
         return (
             <App>
-                <title>Como?</title>
-                <h1 className='left-2'>Como?</h1>
-                <p>
+                <h1 className='tc'>Como?</h1>
+                <p className='indent ml4-ns mt4-ns'>
+                    Utilizamos da inteligência artifical da IBM, o Watson, que em conjunto com um algoritmo de leitura
+                    é possível gerar uma análise sentimental do livro para comparar com o perfil do usuário
+                    e assim realizar recomendações mais acertivas.
+                </p>
+                <p className='indent ml4-ns mt4-ns'>
+                    Para que a análise aconteça, será utilizado a Interface de Programação de Aplicação (API) do
+                    Watson como analisador do sentimento. Utilizando Watson, a inteligência artificial da International
+                    Business Machines (IBM), será possível mapear livros por capítulo analisando os seguintes
+                    sentimentos: desgosto, felicidade, medo, raiva e tristeza.
+                </p>
+                <p className='indent ml4-ns mt4-ns'>
+                    Como base de teste foi utilizado fontes de dados literárias disponíveis na internet – Goodreads e
+                    Project Gutemberg. Ambos contêm uma vasta base de dados literárias, desde livros de época a mais
+                    conhecidos, tais como a coletânea de Harry Potter. Através dos resultados obtidos,
+                    espera-se um nível de acerto de correspondência entre o usuário e livro, aumentando assim,
+                    as chances de recomendação de um livro mais adequado ao perfil e sentimento desejado.
+
                 </p>
             </App>
         )
     }
 }
 
-export default Us;
+export default How;
--- src/pages/Us/index.js
+++ src/pages/Us/index.js
@@ -7,14 +7,16 @@ class Us extends React.Component {
     render() {
         return (
             <App>
-                <title>Nós</title>
-                <h1 className='left-2'>Nós</h1>
-                <p>Somos um grupo de quatro alunos, Guilherme Bruno, Gustavo Santos, Henrique Guerra e Willian  do curso de ciência da computação da unip Marquês,
-                    que como projeto de conclusão de curso escolhemos desenvolver esse Web App.
+                <h1 className='tc'>Nós</h1>
+                <p className='indent ml4-ns mt4-ns'>
+                    Somos um grupo de quatro alunos, Guilherme Bruno, Gustavo Santos, Henrique Guerra e William Souza,
+                    do curso de ciência da computação da UNIP Marquês, onde escolhemos como projeto de
+                    conclusão de curso desenvolver esse Web App.
                 </p>
-                <p>
-                    A ideia surgiu do gosto dos alunos do grupo pela leitura e da vontade de aplicar a tecnologia
-                    em algo tão simples como o ato de recomendar um livro.
+                <p className='indent ml4-ns mt4-ns'>
+                    A idéia surgiu do gosto dos integrantes do grupo pela leitura e da vontade de aplicar a tecnologia
+                    em algo tão simples como o ato de recomendar um livro. Todos do grupo têm bastante contato com a leitura,
+                    seja ela para estudo ou apenas lazer, influenciando muito e nos motivando a levar esse projeto até o fim.
                 </p>
             </App>
         )
