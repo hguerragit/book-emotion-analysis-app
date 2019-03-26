051140ed288bc85734b9ee39882d363a8c647575
Henrique Guerra <hfranca@outlook.com>
2019-03-26 15:17:40 -0300

    create Shelfer

---
+++ src/components/custom/Shelfer/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import RoundIcon from '../RoundIcon';
+
+import { clickAddBookToBooklist } from '../../utils/actions';
+
+const mapStateToProps = ({ book }) => ({
+	...book,
+});
+
+const mapDispatchToProps = dispatch => ({
+	handleBookListAddClick: (
+		userId, 
+		bookId, 
+		plataform, 
+		status
+	) => dispatch(clickAddBookToBooklist(userId, bookId, plataform, status))
+});
+
+class Shelfer extends React.Component {
+	render() {
+		const {
+			classButton,
+			classIcon,
+			family,
+			icon,
+			title,
+			handleBookListAddClick,
+			action,
+			id,
+			userId,
+			plataforms
+		} = this.props;
+
+		return (
+			<RoundIcon
+                classButton={classButton}
+                classIcon={classIcon}
+                family={family}
+                icon={icon}
+                title={title}
+                onClick={() => handleBookListAddClick(userId, id, plataforms, action)}   
+            />
+		);
+	}
+}
+
+const connectPropsWith = connect(mapStateToProps, mapDispatchToProps);
+export default connectPropsWith(Shelfer);
